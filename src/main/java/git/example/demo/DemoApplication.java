2c1c46c9367e024b8664498b843d46e120992c6c
Backend <dhundhoobackend@gmail.com>
2024-09-25 12:08:40 +0530

    changes

--- src/main/java/git/example/demo/DemoApplication.java
+++ src/main/java/git/example/demo/DemoApplication.java
@@ -1,11 +1,21 @@
 package git.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Update;
 
 @SpringBootApplication
+@Configuration
 public class DemoApplication  implements CommandLineRunner {
+
+	Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+	MongoTemplate mongoTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -13,7 +23,10 @@ public class DemoApplication  implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Added by DEV - Command line runner");
-		System.out.println("Main branch");
+		logger.info("Added by DEV - Command line runner");
+		logger.info("Dev branch");
+
+		logger.info("App started by Dev");
+
 	}
 }
